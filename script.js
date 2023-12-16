acd2fc621898c0948b94301557a29216b850f950
eIi0 <100869694+eIi0@users.noreply.github.com>
2023-12-16 11:45:31 +0100

    Fix error lors du GET api

--- script.js
+++ script.js
@@ -16,22 +16,19 @@ function showPosition(position) {
 function getweatherinfo(position){
     let url = "https://api.open-meteo.com/v1/forecast?latitude="+ position.coords.latitude +"&longitude="+ position.coords.longitude+"&current_weather=true"
 
-    console.log(url)
+    const req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.send();
 
-    const Http = new XMLHttpRequest();
-    Http.open("GET", url);
-    Http.send();
+    req.onload  = function() {
+        var jsonResponse = JSON.parse(req.responseText);
+        parsingJSONweather(jsonResponse)
+     };
 
-    //console.log(Http)
-
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText)
-        var parseresponse = JSON.parse(Http.responseText)
-        console.log(parseresponse)
-    }
 }
 
 function parsingJSONweather(response){
-    let parseresponses = JSON.parse(response);
     console.log(response.latitude);
+
+
 }
